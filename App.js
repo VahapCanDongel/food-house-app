93277e2a47bc9e8f797cdb15c88e9891f8d5ff3a
v-Alqorith <vahapcandongel@hotmail.com>
2023-05-21 23:21:34 +0100

    Tested some styling

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Lets see if it is going to work</Text>
+
+      <Pressable style={styles.myButton}>
+        <Text style={styles.buttonText}>Click me</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +16,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  myButton: {
+    backgroundColor: "#428af5",
+    flex: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 150,
+    padding: 8,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: "#ffff",
+    fontSize: 16,
   },
 });
